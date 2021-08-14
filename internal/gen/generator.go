80ad523b0c8975857d97a2a2dea31e22f53fd3e5
Geektree0101 <h2s1880@gmail.com>
2021-08-14 23:08:54 +0900

    generator constructor

--- internal/gen/generator.go
+++ internal/gen/generator.go
@@ -1,9 +1,63 @@
 package gen
 
-type Generator struct {
+import (
+	"io/ioutil"
+	"strings"
+
+	"github.com/Geektree0101/clean-swift-scaffold/internal/model"
+	"gopkg.in/yaml.v2"
+)
+
+type GeneratorConfig struct {
+	Name           string
+	UsecasesString string
+	SourcePath     string
+	TestPath       string
+	ConfigFilePath string
 }
 
-func NewGenerator() *Generator {
+type Generator struct {
+	name       string
+	usecases   []string
+	sourcePath string
+	testPath   string
+	config     *model.Config
+}
 
-	return &Generator{}
+func NewGenerator(config GeneratorConfig) (*Generator, error) {
+
+	return &Generator{
+		name:       config.Name,
+		usecases:   strings.Split(config.UsecasesString, ","),
+		sourcePath: config.SourcePath,
+		testPath:   config.TestPath,
+		config:     readConfig(config.ConfigFilePath),
+	}, nil
+}
+
+func readConfig(path string) *model.Config {
+
+	content, err := ioutil.ReadFile(path)
+
+	if err != nil {
+		return &model.Config{
+			Org:          "Unknown",
+			Copyright:    "Geektree0101",
+			TemplatePath: "./templates",
+		}
+	}
+
+	config := &model.Config{}
+
+	err = yaml.Unmarshal(content, &config)
+
+	if err != nil {
+		return &model.Config{
+			Org:          "Unknown",
+			Copyright:    "Geektree0101",
+			TemplatePath: "./templates",
+		}
+	}
+
+	return config
 }
