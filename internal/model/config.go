e90d5a90c49c635c326e7a9fe8844f0bdd6b6c8d
Geektree0101 <h2s1880@gmail.com>
2021-08-14 22:49:19 +0900

    add config.go

--- internal/model/config.go
+++ internal/model/config.go
@@ -1,4 +1,16 @@
 package model
 
+// Header components
+//
+//  __USECASE__ViewControllerTests.swift
+//  __ORGANIZATION__Tests
+//
+//  Created by clean-swift-scaffold on __DATE__.
+//  Copyright © __YEAR__ __COPYRIGHT__. All rights reserved.
+//
+
 type Config struct {
+	Org          string `yaml:"organization"`
+	Copyright    string `yaml:"copyright"`
+	TemplatePath string `yaml:"template_path"`
 }
