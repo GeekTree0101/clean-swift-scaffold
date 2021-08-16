d4e4bc58982f14e6c72d4097493b57f129e521a1
Geektree0101 <h2s1880@gmail.com>
2021-08-16 21:23:19 +0900

    add NewSourceConverte constructor logics

--- internal/converter/source.go
+++ internal/converter/source.go
@@ -1,9 +1,26 @@
 package converter
 
+import (
+	"time"
+)
+
 type SourceConverter struct {
+	usecases   []string
+	sourcePath string
+	testPath   string
+	date       time.Time
 }
 
-func NewSourceConverter() *SourceConverter {
+func NewSourceConverter(
+	usecases []string,
+	sourcePath string,
+	testPath string,
+	date time.Time) *SourceConverter {
 
-	return &SourceConverter{}
+	return &SourceConverter{
+		usecases:   usecases,
+		sourcePath: sourcePath,
+		testPath:   testPath,
+		date:       date,
+	}
 }
