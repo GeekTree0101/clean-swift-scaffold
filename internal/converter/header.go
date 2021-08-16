485e3febd264b6f2037a1bc6f7806655a03b8028
Geektree0101 <h2s1880@gmail.com>
2021-08-16 21:19:11 +0900

    unit test header.render:

--- internal/converter/header.go
+++ internal/converter/header.go
@@ -1,9 +1,45 @@
 package converter
 
+import (
+	"fmt"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/Geektree0101/clean-swift-scaffold/internal/model"
+)
+
 type HeaderConverter struct {
+	org       string
+	copyright string
+	date      time.Time
 }
 
-func NewHeaderConverter() *HeaderConverter {
+func NewHeaderConverter(
+	config *model.Config,
+	date time.Time) *HeaderConverter {
 
-	return &HeaderConverter{}
+	return &HeaderConverter{
+		org:       config.Org,
+		copyright: config.Copyright,
+		date:      date,
+	}
+}
+
+func (header *HeaderConverter) Render(source string, usecase string) string {
+
+	day := header.date.Day()
+	month := int(header.date.Month())
+	year := header.date.Year()
+
+	dateStr := fmt.Sprintf("%d/%d/%d", day, month, year)
+
+	var replacedSource string = source
+	replacedSource = strings.ReplaceAll(replacedSource, "__USECASE__", usecase)
+	replacedSource = strings.ReplaceAll(replacedSource, "__ORGANIZATION__", header.org)
+	replacedSource = strings.ReplaceAll(replacedSource, "__DATE__", dateStr)
+	replacedSource = strings.ReplaceAll(replacedSource, "__YEAR__", strconv.Itoa(year))
+	replacedSource = strings.ReplaceAll(replacedSource, "__COPYRIGHT__", header.copyright)
+
+	return replacedSource
 }
