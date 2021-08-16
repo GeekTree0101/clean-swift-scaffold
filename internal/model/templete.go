aa69cde87a347a398db53170da9cb776803efd98
Geektree0101 <h2s1880@gmail.com>
2021-08-16 22:02:35 +0900

    add templete

---
+++ internal/model/templete.go
@@ -0,0 +1,41 @@
+package model
+
+const (
+	UsecaseTemplate string = `
+		enum __USECASE__ {
+
+			struct Reuest {
+
+			}
+
+			struct Response {
+
+			}
+
+			struct ViewModel {
+
+			}
+		}
+	`
+	// interactor
+	InteractorImplTemplate string = `
+	  func __METHOD_NAME__(request: __SCENE_NAME__.__USECASE__.Request) {
+
+		}
+	`
+	InteractorInterpaceTemplate string = `func __METHOD_NAME__(request: __SCENE_NAME__.__USECASE__.Request)`
+	// presenter
+	PresenterImplTemplate string = `
+	  func present__USECASE__(response: __SCENE_NAME__.__USECASE__.Response) {
+
+		}
+	`
+	PresenterInterpaceTemplate string = `func present__USECASE__(response: __SCENE_NAME__.__USECASE__.Response)`
+	// display
+	displayImplTemplate string = `
+	  func display__USECASE__(viewModel: __SCENE_NAME__.__USECASE__.ViewModel) {
+
+		}
+	`
+	displayInterpaceTemplate string = `func display__USECASE__(viewModel: __SCENE_NAME__.__USECASE__.ViewModel)`
+)
